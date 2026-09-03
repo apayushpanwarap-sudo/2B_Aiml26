65a5884a609229d58b3f3fbc58bba0efed7f3274
apayushpanwarap <ap.ayushpanwar.ap@gmail.com>
2026-09-03 12:07:35 +0530

    arrorfunction

---
+++ formpage/arrowfun.js
@@ -0,0 +1,26 @@
+// function hello(){
+//     console.log("hello world");
+// }
+// let hello2=function(){
+//     console.log("hello world");
+// }
+// hello3=()=>{console.log("hello world")};
+// hello3();
+// hello2();
+// hello();
+
+// function physics(marks){
+//     return `${marks}`
+
+// }
+physics=(marks)=>{return `${marks}`};
+console.log(physics(90));
+// *
+// **
+// ***
+// function pattern(n){
+//     for(let i=1;i<=n;i++){
+//         console.log("*".repeat(i));
+//     }
+// }
+// pattern(3);
