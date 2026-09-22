132b3ce4ac5b1631288d30bf70b665fb7d716330
apayushpanwarap <ap.ayushpanwar.ap@gmail.com>
2026-09-22 11:37:56 +0530

    question3

--- classpractice/ques3.js
+++ classpractice/ques3.js
@@ -1,3 +1,5 @@
+// 3. Employee and Manager Payroll
+//  Create a base class Employee containing id, name, and basicSalary. Initialize them using a constructor and create a method calculateSalary() that returns the basic salary. Create a derived class Manager that adds an incentive property. Use super() to initialize inherited properties and override calculateSalary() so that a manager's total salary is calculated as basicSalary + incentive. Create objects of both classes and display their salaries.   Concepts Covered: Inheritance, extends, super(), Constructor, Method Overriding
 
 class Employee {
     constructor(id, name, basicSalary) {
