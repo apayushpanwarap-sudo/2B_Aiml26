a73a40623cd5fb47501bf29aa5a3fe4fb1f0e383
apayushpanwarap <ap.ayushpanwar.ap@gmail.com>
2026-09-22 11:35:18 +0530

    question1

--- classpractice/ques1.js
+++ classpractice/ques1.js
@@ -1,3 +1,6 @@
+// 1. University Student Management 
+// Create a class Student having rollNo, name, and marks. Initialize these properties using a constructor. Create an instance method displayResult() that displays student details and determines whether the student has passed. Create a static property/method to maintain and display the total number of Student objects created. Create at least three Student objects and demonstrate all methods.   Concepts Covered: Class, Object, Constructor, Instance Method, Static Method
+
 class Student {
 
     static totalStudents = 0;
