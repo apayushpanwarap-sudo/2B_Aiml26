3898ad5082529e8e149efbb780587046f246c1b2
apayushpanwarap <ap.ayushpanwar.ap@gmail.com>
2026-09-22 11:12:33 +0530

    question4

---
+++ classpractice/ques4.js
@@ -0,0 +1,57 @@
+class Product {
+    constructor(productId, productName, price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+    }
+
+  
+    getDiscountedPrice(discount) {
+        return this.price - (this.price * discount / 100);
+    }
+
+    display() {
+        console.log(`Product ID: ${this.productId}`);
+        console.log(`Product Name: ${this.productName}`);
+        console.log(`Price: Rs. ${this.price}`);
+    }
+
+
+    static compareProducts(p1, p2) {
+        if (p1.price > p2.price) {
+            console.log(`${p1.productName} has the higher price.`);
+        } else if (p2.price > p1.price) {
+            console.log(`${p2.productName} has the higher price.`);
+        } else {
+            console.log("Both products have the same price.");
+        }
+    }
+}
+
+
+class Electronics extends Product {
+    constructor(productId, productName, price, warranty) {
+        super(productId, productName, price);
+        this.warranty = warranty;
+    }
+
+
+    display() {
+        super.display();
+        console.log(`Warranty: ${this.warranty} years`);
+    }
+}
+
+
+let p1 = new Product(101, "Shoes", 2000);
+let p2 = new Electronics(102, "Laptop", 60000, 2);
+
+p1.display();
+console.log(`Discounted Price: Rs. ${p1.getDiscountedPrice(10)}`);
+
+console.log("\nElectronics Details:");
+p2.display();
+console.log(`Discounted Price: Rs. ${p2.getDiscountedPrice(15)}`);
+
+console.log("\nPrice Comparison:");
+Product.compareProducts(p1, p2);
